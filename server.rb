d7c97a34bc7c01054c0614dcc363dd476378f1a8
WJLIDDY <wjl36@case.edu>
2017-02-22 14:18:51 -0500

    Improve server test architecture

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'socket'
 class Server
   def initialize
-    # TODO: don't abort threads on exception, handle 'em
+    # TODO: In production. don't abort threads on exception, handle 'em
     Thread.abort_on_exception = true
     @groups = {}
   end
@@ -16,25 +16,26 @@ class Server
   end
 
   def send_ok(socket)
-     socket.print ({"response" => "ok"}.to_json + "\n")
+     socket.puts ({"response" => "ok"}.to_json)
      socket.flush
   end
 
   def send_error(socket, message)
-     socket.print ({"response" => "error", "error message" => message}.to_json + "\n")
+     socket.puts ({"response" => "error", "error message" => message}.to_json )
      socket.flush
   end
 
-  def bandleader_loop(user, socket)
+  def bandleader_loop(user, socket, group_name)
     group = Group.new(user)
-    groups[request['group name']] = group
+    @groups[group_name] = group
+    send_ok(socket)
     loop do
       # ...
     end
   end
 
-  def group_member_loop(user, socket)
-    group = groups[request['group name']]
+  def group_member_loop(user, socket, group_name)
+    group = @groups[group_name]
     group.members << group
     # Group members are passive. The only thing they can do is quit.
     # And if that happens, I remove them from the group.
@@ -42,44 +43,58 @@ class Server
     loop { sleep 300 }
   end
 
-  def launch(new_thread = false)
-    if(new_thread)
-      Thread.new {run_server}
+
+
+
+  # If "test" is enabled, returns the sockets so they can be closed.
+  def launch(test = false)
+    if(test)
+      #open two ports for the server to test multiple connections
+      t1 = TCPServer.open(44106)
+      t2 = TCPServer.open(44107)
+      Thread.new{run_server(t1)}
+      Thread.new{run_server(t2)}
+
+      return [t1, t2]
     else
-      run_server
+      run_server(TCPServer.open(44106))
     end
   end
 
-  def run_server
-    server = TCPServer.open(44106)
-    loop do
-      Thread.start(server.accept) do |socket|
-        puts 'Connection Recieved.'
-        # Await the first JSON packet to tell what room they want to join,
-        # or if they want to create.
-        begin
-          request = recv_json(socket)
-        rescue JSON::ParserError => e
-          send_error(socket, e.message)
-          request = nil
-        end
+  def run_server(server)
+    begin
+      loop do
+        Thread.start(server.accept) do |socket|
+          puts 'Connection Recieved.'
+          # Await the first JSON packet to tell what room they want to join,
+          # or if they want to create.
+          begin
+            request = recv_json(socket)
+          rescue JSON::ParserError => e
+            send_error(socket, e.message)
+            request = nil
+          end
 
-        # Make sure the proper fields are there
-        if(!request.nil? && (request['user name'].nil? || request['request'].nil?))
-          send_error(socket, "JSON was well formed but missing username or request field")
-          request = nil
-        end
+          # Make sure the proper fields are there
+          if(!request.nil? && (request['user name'].nil? || request['request'].nil?))
+            send_error(socket, "JSON was well formed but missing username or request field")
+            request = nil
+          end
 
-        if(!request.nil?)
-          # Create the user
-          user = User.new(request['user name'], socket)
-          # Create or join the group.
-          bandleader_loop(user, socket) if request['request'] == 'create group'
-          group_member_loop(user, socket) if request['request'] == 'join group'
+          if(!request.nil?)
+            # Create the user
+            user = User.new(request['user name'], socket)
+            # Create or join the group.
+            bandleader_loop(user, socket, request['group name']) if request['request'] == 'create group'
+            group_member_loop(user, socket, request['group name']) if request['request'] == 'join group'
+          end
+          puts 'Connection Terminated.'
+          socket.close
         end
-        puts 'Connection Terminated.'
-        socket.close
       end
+    # Only called if server forcefully killed.
+    rescue IOError => e
+      #puts "#{e.message}"
     end
   end
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,11 +1,23 @@
 require_relative "../server"
+
+
 describe Server do
   it "throws an error for invalid JSON" do
-    Server.new.launch(true)
+    s_threads = Server.new.launch(true)
     socket = TCPSocket.open("localhost", 44106)
     socket.print("{ i'm not a valid json \n")
-    response =  JSON.parse(socket.read)
+    response =  JSON.parse(socket.gets)
     expect(response["response"]).to eq("error")
-    puts "got #{response}"
+    socket.close
+    s_threads.each { |s| s.close}
+  end
+
+  it "allows group creation" do
+    s_threads = Server.new.launch(true)
+    socket = TCPSocket.open("localhost", 44106)
+    socket.print ( {"request" =>"create group","group name" => "stp","user name" => "scott"}.to_json + "\n" ) 
+    response =  JSON.parse(socket.gets)
+    expect(response["response"]).to eq("ok")
+    s_threads.each { |s| s.close}
   end
 end
